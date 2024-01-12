12c4b1cde0405ff563dced19fb1e55b392767caf
jimmyxuexue <1002661758@qq.com>
2024-01-12 10:54:20 +0800

    :bug: 默认路由优化

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.tsx'
 import { Theme } from '@radix-ui/themes'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import '@radix-ui/themes/styles.css'
 import './main.css'
 import { Category } from './page/category/index.tsx'
@@ -22,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 							<Route path="nokia" element={<Nokia />} />
 							<Route path="picColor" element={<PicColor />} />
 						</Route>
+						<Route path="*" element={<Navigate to="home/category" replace />} />
 					</Routes>
 				</Router>
 				<Toast />
